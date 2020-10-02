b6a9f47b77187936ddd0c579481cd1ebc08af609
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-02 12:55:06 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -119,7 +119,7 @@ async def rename_video(bot, update):
             await bot.send_video(
                 chat_id=update.chat.id,
                 video=new_file_name,
-                duration=duration,
+                #duration=duration,
                 thumb=thumb_image_path,
                 caption=f"<b>{file_name} \n\nShare and Support\n\n@SerialCoIn</b>",
                 # reply_markup=reply_markup,
