655ed22ffb1d16797542bed3be977c1e7eacabbf
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-11 01:49:52 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -46,7 +46,7 @@ I am Turbo Renamer bot ✍ by <a href='https://t.me/JAsuran123'><b>Asuran</b></a
 Support Group : <b>@SerialCoIn</b>"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "🙆 Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for mor information. "
     REPLY_TO_DOC_FOR_RENAME_VIDEO = "🙋 Reply to a Telegram media to `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for mor information."
-    REPLY_TO_DOC_FOR_SCSS = "🙋 Reply to a Telegram media to `/gshoot` Get Screenshots
+    REPLY_TO_DOC_FOR_SCSS = "🙋 Reply to a Telegram media to `/gshoot` Get Screenshots"
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
 Free users only 1 request per 30 minutes.
