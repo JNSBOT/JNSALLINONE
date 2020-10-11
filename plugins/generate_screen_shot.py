efb55dc549d8c880980f0a719a76f2dbee1045f7
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-12 03:54:28 +0530

    Update generate_screen_shot.py

--- plugins/generate_screen_shot.py
+++ plugins/generate_screen_shot.py
@@ -83,7 +83,7 @@ async def generate_screen_shot(bot, update):
             media_album_p = []
             if images is not None:
                 i = 0
-                caption = "Share and Support\n\n@SerialCoIn"
+                caption=f"<b>{file_name} \n\nShare and Support\n\n@SerialCoIn</b>"
                 for image in images:
                     if os.path.exists(image):
                         if i == 0:
