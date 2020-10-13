5048cb6b67dd147d56d27a99e5079ec8fead0660
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-13 22:21:43 +0530

    Update rename_v.py

--- plugins/rename_v.py
+++ plugins/rename_v.py
@@ -71,27 +71,21 @@ async def rename_video(bot, update):
             )
         )
         if the_real_download_location is not None:
-           try:
-             #await bot.edit_message_text(
-                 #text=Translation.SAVED_RECVD_DOC_FILE,
-                 #chat_id=update.chat.id,
-                 #message_id=a.message_id
-             )
-          except:
-             pass
-          new_file_name = download_location + file_name
-          os.rename(the_real_download_location, new_file_name)
-          await bot.edit_message_text(
-              text=Translation.UPLOAD_START,
-              chat_id=update.chat.id,
-              message_id=a.message_id
-              )
-            # don't care about the extension
-           # await bot.edit_message_text(
-              #  text=Translation.UPLOAD_START,
-             #   chat_id=update.chat.id,
-            #    message_id=a.message_id
-          #  )
+            try:
+                await bot.edit_message_text(
+                    text=Translation.SAVED_RECVD_DOC_FILE,
+                    chat_id=update.chat.id,
+                    message_id=a.message_id
+                )
+            except:
+                pass
+            new_file_name = download_location + file_name
+            os.rename(the_real_download_location, new_file_name)
+            await bot.edit_message_text(
+                text=Translation.UPLOAD_START,
+                chat_id=update.chat.id,
+                message_id=a.message_id
+                )
             logger.info(the_real_download_location)
             # get the correct width, height, and duration for videos greater than 10MB
             # ref: message from @BotSupport
@@ -150,7 +144,7 @@ async def rename_video(bot, update):
                 )
             )
             try:
-                os.remove(the_real_download_location)
+                os.remove(new_file_name)
               #  os.remove(thumb_image_path)
             except:
                 pass
