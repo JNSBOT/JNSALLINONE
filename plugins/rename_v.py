8533bc3e91f2b2aa6d62b6dc5ac2665aaec98494
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-13 19:00:27 +0530

    Update rename_v.py

--- plugins/rename_v.py
+++ plugins/rename_v.py
@@ -72,14 +72,14 @@ async def rename_video(bot, update):
             )
         )
         if the_real_download_location is not None:
-            try:
-                await bot.edit_message_text(
-                text=Translation.SAVED_RECVD_DOC_FILE,
-                chat_id=update.chat.id,
-                message_id=a.message_id
-            )
-            except:
-                pass
+          #  try:
+             #   await bot.edit_message_text(
+              #  text=Translation.SAVED_RECVD_DOC_FILE,
+            #    chat_id=update.chat.id,
+             #   message_id=a.message_id
+          #  )
+          #  except:
+            #    pass
             new_file_name = download_location + file_name
             os.rename(the_real_download_location, new_file_name)
             await bot.edit_message_text(
