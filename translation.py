ff37fbd4dc7fbd15a0e5511cb1a39b8f663ddee3
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-31 14:41:17 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,7 +7,7 @@ I Can rename ✍ with custom thumbnail and upload as video/file
 
 Type /help for more details."""
 RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
-    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
+    #UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
