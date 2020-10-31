49a6f5999ce8961f42626509c2d22a9c4d77da4b
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-31 21:06:26 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -102,7 +102,7 @@ async def youtube_dl_call_back(bot, update):
         chat_id=update.message.chat.id,
         message_id=update.message.message_id
     )
-    description = Translation1.CUSTOM_CAPTION_UL_FILE
+    #description = Translation1.CUSTOM_CAPTION_UL_FILE
     if "fulltitle" in response_json:
         description = response_json["fulltitle"][0:1021]
         # escape Markdown and special characters
