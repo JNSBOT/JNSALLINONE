7399c7af4d08a25fbd559692265626708d061ea7
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-31 21:19:38 +0530

    Update translation1.py

--- translation1.py
+++ translation1.py
@@ -30,50 +30,6 @@ NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
 NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
 USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
 
-class Translation(object):
-   START_TEXT = """<i>Vanakkam</i> <i><b>{}</b></i>,
-
-Welcome to Our <a href='https://t.me/turboremaxbot'><b>Turbo Renamer ⚡</b></a> bot
-
-I Can rename ✍ with custom thumbnail and upload as video/file
-
-Type /help for more details."""
-RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
-ABS_TEXT = " Please don't be selfish."
-UPGRADE_TEXT = "There is no upgrade plan till now it will be added in future"
-DOWNLOAD_START_VIDEO = "Downloading.....⤵️"
-DOWNLOAD_START = "Downloading.....⤵️"
-UPLOAD_START_VIDEO = "Uploading.....⤴️"
-UPLOAD_START = "Uploading.....⤴️"
-RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations.I can't do anything for that 🤷‍♂️."
-AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [Turbo Renamer](https://t.me/JAsuran123)'s bot.**"
-AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
-NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://t.me/JAsuran123'>Turbo Renamer</a>"
-SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved 🤗 permanently. This image will be used in the File."
-DEL_ETED_CUSTOM_THUMB_NAIL = "🤔 Custom thumbnail cleared succesfully."
-FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "🤨 Media cleared succesfully."
-SAVED_RECVD_DOC_FILE = "👹 Document Downloaded Successfully."
-CUSTOM_CAPTION_UL_FILE = "<b>\nShare and Support \n\n@SerialCoIn</b>"
-NO_CUSTOM_THUMB_NAIL_FOUND = "😠 No Custom ThumbNail found."
-NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
-USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-HELP_USER = """Vanakkam <b><i>{}</i></b>, 
-
-I am Turbo Renamer bot ✍ by <a href='https://t.me/JAsuran123'><b>Asuran</b></a>
-    
-1. Send Me A Thumbnail.
-
-2. Send me the file to be Renamed.
-
-3. Reply to that message with <code>/rename new name.extension</code>. with custom thumbnail support.\n(upload as file)
-
-4. Reply to that message with <code>/renamev new name.extension</code>. with custom thumbnail support.\n(uploading as Video)
-
-5. Reply to that message with <code>/gshoot</code> Uploading as Screenshots
-   
-<b>Thanks to <i><a href="https://t.me/JAsuran123">Turbo Renamer 🙇</a></i> for his source code. check /about for source code</b>
-
 --------
 
 Support Group : <b>@SerialCoIn</b>"""
