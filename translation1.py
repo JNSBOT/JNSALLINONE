4439b32425a019e3c97daf85a11b510221d9dc81
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-31 22:15:27 +0530

    Update translation1.py

--- translation1.py
+++ translation1.py
@@ -25,7 +25,7 @@ SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will 
 DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
 FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
 SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-#CUSTOM_CAPTION_UL_FILE = "Share and Support\n\n@SerialCoIn"
+CUSTOM_CAPTION_UL_FILE = "Share and Support\n\n@SerialCoIn"
 NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
 NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
 USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
