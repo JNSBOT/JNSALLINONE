528b4bd9d472294e3876a077f4e3e88aa8fffe51
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-11-01 06:17:16 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -35,7 +35,7 @@ async def generate_custom_thumbnail(bot, update):
         await bot.delete_messages(
             chat_id=update.chat.id,
             message_ids=update.message_id,
-            revoke=True
+            revoke=False
         )
         return
     TRChatBase(update.from_user.id, update.text, "generatecustomthumbnail")
