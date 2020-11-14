e83b5bae3f3b323522ff4670ce3232dce7204659
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-11-14 13:19:35 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -23,7 +23,7 @@ else:
     from config import Config
 
 # the Strings used for this "thing"
-from translation1 import Translation
+from translation import Translation
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
@@ -200,7 +200,7 @@ async def youtube_dl_call_back(bot, update):
                 is_w_f,
                 Config.DEF_WATER_MARK_FILE,
                 300,
-                1
+                9
             )
             logger.info(images)
             await bot.edit_message_text(
@@ -325,7 +325,7 @@ async def youtube_dl_call_back(bot, update):
             media_album_p = []
             if images is not None:
                 i = 0
-                caption = "© @SerialCoIn"
+                caption = "© @MoVieZonLyheRe"
                 if is_w_f:
                     caption = "/upgrade to Plan D to remove the watermark\n© @AnyDLBot"
                 for image in images:
