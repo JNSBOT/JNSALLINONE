257abf1141ad3014f54466634f268be3ccc9a651
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-12-08 01:45:26 +0530

    Update file_convertor.py

--- plugins/file_convertor.py
+++ plugins/file_convertor.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Turbo Renamer 
 
 # the logging things
 import logging
@@ -6,7 +9,6 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 
 import os
-import random
 import time
 
 # the secret configuration specific things
@@ -20,10 +22,10 @@ from translation import Translation
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from pyrogram import Client, Filters
 
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram
-from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
@@ -31,17 +33,26 @@ from hachoir.parser import createParser
 from PIL import Image
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["c2f"]))
-async def convert_to_file(bot, update):
+@pyrogram.Client.on_message(pyrogram.Filters.command(["rename"]))
+async def rename_doc(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
-        await bot.send_message(
+        await bot.delete_messages(
             chat_id=update.chat.id,
-            text=Translation.BANNED_USER_TEXT,
-            reply_to_message_id=update.message_id
+            message_ids=update.message_id,
+            revoke=True
         )
         return
-    TRChatBase(update.from_user.id, update.text, "c2f")
-    if update.reply_to_message is not None:
+    TRChatBase(update.from_user.id, update.text, "rename")
+    if (" " in update.text) and (update.reply_to_message is not None):
+        cmd, file_name = update.text.split(" ", 1)
+        if len(file_name) > 128:
+            await update.reply_text(
+                Translation.IFLONG_FILE_NAME.format(
+                    alimit="128",
+                    num=len(file_name)
+                )
+            )
+            return
         description = Translation.CUSTOM_CAPTION_UL_FILE
         download_location = Config.DOWNLOAD_LOCATION + "/"
         a = await bot.send_message(
@@ -61,82 +72,61 @@ async def convert_to_file(bot, update):
             )
         )
         if the_real_download_location is not None:
-            bot.edit_message_text(
-                text=Translation.SAVED_RECVD_DOC_FILE,
+            try:
+                await bot.edit_message_text(
+                    text=Translation.SAVED_RECVD_DOC_FILE,
+                    chat_id=update.chat.id,
+                    message_id=a.message_id
+                )
+            except:
+                pass
+            new_file_name = download_location + file_name
+            os.rename(the_real_download_location, new_file_name)
+            await bot.edit_message_text(
+                text=Translation.UPLOAD_START,
                 chat_id=update.chat.id,
                 message_id=a.message_id
-            )
-            # don't care about the extension
-           # await bot.edit_message_text(
-              #  text=Translation.UPLOAD_START,
-             #   chat_id=update.chat.id,
-            #    message_id=a.message_id
-          #  )
-           # logger.info(the_real_download_location)
+                )
             logger.info(the_real_download_location)
             thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
             if not os.path.exists(thumb_image_path):
                 thumb_image_path = None
             else:
-            # get the correct width, height, and duration for videos greater than 10MB
-            # ref: message from @BotSupport
                 width = 0
                 height = 0
-            #duration = 0
-           # metadata = extractMetadata(createParser(the_real_download_location))
-           # if metadata.has("duration"):
-               # duration = metadata.get('duration').seconds
-           # thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-           # if not os.path.exists(thumb_image_path):
-               # thumb_image_path = await take_screen_shot(
-                  #  the_real_download_location,
-                   # os.path.dirname(the_real_download_location),
-                  #  random.randint(
-                     #   0,
-                    #    duration - 1
-                  #  )
-               # )
-           # logger.info(thumb_image_path)
-            # 'thumb_image_path' will be available now
-            metadata = extractMetadata(createParser(thumb_image_path))
-            if metadata.has("width"):
-                width = metadata.get("width")
-            if metadata.has("height"):
-                height = metadata.get("height")
-            # get the correct width, height, and duration for videos greater than 10MB
-            # resize image
-            # ref: https://t.me/PyrogramChat/44663
-            # https://stackoverflow.com/a/21669827/4723940
-            Image.open(thumb_image_path).convert("RGB").save(thumb_image_path)
-            img = Image.open(thumb_image_path)
-            # https://stackoverflow.com/a/37631799/4723940
-            # img.thumbnail((90, 90))
-            img.resize((320, height))
-            img.save(thumb_image_path, "JPEG")
-            # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
-            # try to upload file
+                metadata = extractMetadata(createParser(thumb_image_path))
+                if metadata.has("width"):
+                    width = metadata.get("width")
+                if metadata.has("height"):
+                    height = metadata.get("height")
+                # resize image
+                # ref: https://t.me/PyrogramChat/44663
+                # https://stackoverflow.com/a/21669827/4723940
+                Image.open(thumb_image_path).convert("RGB").save(thumb_image_path)
+                img = Image.open(thumb_image_path)
+                # https://stackoverflow.com/a/37631799/4723940
+                # img.thumbnail((90, 90))
+                img.resize((320, height))
+                img.save(thumb_image_path, "JPEG")
+                # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
             c_time = time.time()
             await bot.send_document(
                 chat_id=update.chat.id,
-                document=the_real_download_location,
-                caption=description,
-              #  duration=duration,
-                #width=width,
-               # height=height,
-               # supports_streaming=True,
-                # reply_markup=reply_markup,
+                document=new_file_name,
                 thumb=thumb_image_path,
+                caption=f"<b>{file_name} \n\nShare and Support\n\n@SerialCoIn</b>",
+                # reply_markup=reply_markup,
                 reply_to_message_id=update.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
                 progress_args=(
                     Translation.UPLOAD_START,
-                    a,
+                    a, 
                     c_time
                 )
             )
             try:
-                os.remove(the_real_download_location)
-              #  os.remove(thumb_image_path)
+                os.remove(new_file_name)
+                #os.remove(thumb_image_path)
             except:
                 pass
             await bot.edit_message_text(
@@ -148,6 +138,6 @@ async def convert_to_file(bot, update):
     else:
         await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.REPLY_TO_DOC_FOR_C2V,
+            text=Translation.REPLY_TO_DOC_FOR_RENAME_FILE,
             reply_to_message_id=update.message_id
         )
