fb26f667d14f0f7c29daffe5a4f7ff0dde8887b8
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-12-14 14:28:17 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -45,10 +45,10 @@ async def rename_doc(bot, update):
     TRChatBase(update.from_user.id, update.text, "rename")
     if (" " in update.text) and (update.reply_to_message is not None):
         cmd, file_name = update.text.split(" ", 1)
-        if len(file_name) > 128:
+        if len(file_name) > 512:
             await update.reply_text(
                 Translation.IFLONG_FILE_NAME.format(
-                    alimit="128",
+                    alimit="512",
                     num=len(file_name)
                 )
             )
