4f858102c87fbb20f4774f95a5f567ed75761be9
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-12-14 07:42:51 +0530

    Update split_large_files.py

--- plugins/split_large_files.py
+++ plugins/split_large_files.py
@@ -18,12 +18,16 @@ import time
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
+from Sample_config import (
+    MAX_TG_SPLIT_FILE_SIZE
+)
+
 
 # the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+#if bool(os.environ.get("WEBHOOK", False)):
+    #from sample_config import Config
+#else:
+    #from config import Config
 
 # the Strings used for this "thing"
 from translation import Translation
