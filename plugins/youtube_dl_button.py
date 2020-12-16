7bbf681d0a4f57f16b2666405b4296559fbba0c5
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-12-17 01:52:55 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -23,7 +23,7 @@ else:
     from config import Config
 
 # the Strings used for this "thing"
-from translation import Translation
+from translation1 import Translation
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
@@ -34,6 +34,7 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 # https://stackoverflow.com/a/37631799/4723940
 from PIL import Image
+from database.database import *
 
 
 from helper_funcs.help_Nekmo_ffmpeg import generate_screen_shots
