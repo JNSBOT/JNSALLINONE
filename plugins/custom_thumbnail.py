bbb1942fb15538269b26e2accd251c8518bb085f
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-12-17 10:08:07 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -28,7 +28,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.chat_base import TRChatBase
 
-import database.database as sql
+#import database.database as sql
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["generatecustomthumbnail"]))
