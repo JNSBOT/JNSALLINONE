f6567a2d8ac52c1d31d10981408e4c1db4e30230
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-12-17 07:18:55 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -23,7 +23,7 @@ else:
     from config import Config
 
 # the Strings used for this "thing"
-from translation1 import Translation
+from translation import Translation
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
