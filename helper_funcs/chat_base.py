c87547c42d2e662ffdd4faa5e984dacb1e61db38
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-01-16 15:43:28 +0530

    Update chat_base.py

--- helper_funcs/chat_base.py
+++ helper_funcs/chat_base.py
@@ -20,7 +20,7 @@ else:
 from translation import Translation
 #from translation1 import Translation
 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
+from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
 
 # the Telegram trackings
 from chatbase import Message
