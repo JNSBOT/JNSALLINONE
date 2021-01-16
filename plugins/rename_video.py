2ef9e6dcc58933a3098a9567305fbad80fe66f6e
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-01-16 15:24:22 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 import os
 import time
 import random
+import asyncio
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
@@ -32,28 +33,77 @@ from hachoir.parser import createParser
 # https://stackoverflow.com/a/37631799/4723940
 from PIL import Image
 #from database.database import *
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
 
+async def force_name(bot, message):
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["rename_video"]))
-async def rename_video(bot, update):
-    if update.from_user.id in Config.BANNED_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
+    await bot.send_message(
+        message.reply_to_message.from_user.id,
+        "Enter new name for media\n\nNote : Extension not required",
+        reply_to_message_id=message.reply_to_message.message_id,
+        reply_markup=ForceReply(True)
+    )
+
+@Client.on_message(filters.private & filters.reply & filters.text)
+async def cus_name(bot, message):
+    
+    if (message.reply_to_message.reply_markup) and isinstance(message.reply_to_message.reply_markup, ForceReply):
+        asyncio.create_task(rename_doc(bot, message))     
+    else:
+        print('No media present')
+
+    
+async def rename_doc(bot, message):
+    
+    mssg = await bot.get_messages(
+        message.chat.id,
+        message.reply_to_message.message_id
+    )    
+
+    media = mssg.reply_to_message
+
+    
+    if media.empty:
+        await message.reply_text('Why did you delete that 😕', True)
         return
-    TRChatBase(update.from_user.id, update.text, "rename_video")
-    if (" " in update.text) and (update.reply_to_message is not None):
-        cmd, file_name = update.text.split(" ", 1)
-        if len(file_name) > 512:
-            await update.reply_text(
-                Translation.IFLONG_FILE_NAME.format(
-                    alimit="512",
-                    num=len(file_name)
-                )
-            )
-            return
+        
+    filetype = media.document or media.video or media.audio or media.voice or media.video_note
+    try:
+        actualname = filetype.file_name
+        splitit = actualname.split(".")
+        extension = (splitit[-1])
+    except:
+        extension = "mkv"
+
+    await bot.delete_messages(
+        chat_id=message.chat.id,
+        message_ids=message.reply_to_message.message_id,
+        revoke=True
+    )
+    
+    if message.from_user.id not in Config.BANNED_USERS:
+        file_name = message.text
+
+#@pyrogram.Client.on_message(pyrogram.Filters.command(["rename_video"]))
+#async def rename_video(bot, update):
+    #if update.from_user.id in Config.BANNED_USERS:
+        #await bot.delete_messages(
+            #chat_id=update.chat.id,
+            #message_ids=update.message_id,
+            #revoke=True
+        #)
+        #return
+    #TRChatBase(update.from_user.id, update.text, "rename_video")
+    #if (" " in update.text) and (update.reply_to_message is not None):
+        #cmd, file_name = update.text.split(" ", 1)
+        #if len(file_name) > 512:
+            #await update.reply_text(
+                #Translation.IFLONG_FILE_NAME.format(
+                    #alimit="512",
+                    #num=len(file_name)
+                #)
+            #)
+            #return
         description = Translation.CUSTOM_CAPTION_UL_FILE
         download_location = Config.DOWNLOAD_LOCATION + "/"
         b = await bot.send_message(
